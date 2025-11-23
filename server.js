2e3c6a33c246cecd60dbc40ef4c869696ec353ba
Fabrico Avalos <fabrifj07@gmail.com>
2025-11-22 19:03:01 -0600

    Corregir rutas para despliegue en producción

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const path = require('path');
 
 const app = express();
 
-// CORREGIDO: Servir archivos estáticos desde el directorio raíz del proyecto
+// Servir archivos estáticos desde el directorio raíz
 app.use(express.static(__dirname));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
 
-app.get('/', (req, res) => {
+// Manejar todas las rutas para SPA (Single Page Application)
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const HTTP_PORT = process.env.HTTP_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(HTTP_PORT, () => {
-  console.log(`Servidor HTTP ejecutándose en el puerto ${HTTP_PORT}`);
-  console.log(`Accede a la aplicación en: http://localhost:${HTTP_PORT}`);
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+  console.log(`Accede a la aplicación en: http://localhost:${PORT}`);
 });
